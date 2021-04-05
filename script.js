0fb7cba0f8e7dcb25dfe7dbe7b52bede201264c3
Ajala Fiyinfoluwa John <johnajala204@gmail.com>
2021-04-05 15:11:14 +0100

    added email functions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let emailDOM = document.querySelector('#email');
+let errMsg = document.querySelector('#err-msg');
+let border = document.querySelector('.news-letter');
 
+
+function submit() {
+    let email = document.querySelector('#email').value;
+    let containAt = email.includes('@', 0);
+
+    if (email === '' || !containAt){
+        border.style.border = '2px solid var(--dark-red)';
+        errMsg.style.display = 'block';
+        emailDOM.style.backgroundImage = 'url("images/icon-error.svg")';
+    } else {
+        border.style.border = '2px solid var(--light-pink)';
+        errMsg.style.display = 'block';
+        errMsg.style.color = 'green';
+        errMsg.innerHTML = 'Thanks for Subscribing';
+        emailDOM.style.backgroundImage = ''
+    }
+}
